78c56740ac258840507ac27b503d16b183c3c337
olivier.lediouris <olivier.lediouris@gmail.com>
2021-05-25 07:01:15 -0700

    Small polishing.

--- src/main/java/celestial/almanac/MoonIllumination.java
+++ src/main/java/celestial/almanac/MoonIllumination.java
@@ -27,6 +27,7 @@ public class MoonIllumination {
         Calendar date = Calendar.getInstance(TimeZone.getTimeZone("Etc/UTC")); // Now
         date.set(Calendar.MINUTE, 0);
         date.set(Calendar.SECOND, 0);
+        date.set(Calendar.MILLISECOND, 0);
 
         if (!now) { // Hard coded date
             date.set(Calendar.YEAR, 2020);
@@ -40,15 +41,19 @@ public class MoonIllumination {
 
         // Recalculate
         double deltaT = TimeUtil.getDeltaT(date.get(Calendar.YEAR), date.get(Calendar.MONTH) + 1);
-//		System.out.printf(">> deltaT: %f s\n", deltaT);
         AstroComputer.setDeltaT(deltaT);
+//		System.out.printf(">> deltaT: %f s\n", deltaT);
 
         if (csv) {
             System.out.println("Date\tMoon Illumination (%)\tMoon Phase (\272)");
         }
 
         Calendar until = Calendar.getInstance(TimeZone.getTimeZone("Etc/UTC")); // End date.
-        until.add(Calendar.YEAR, 1);
+        until.add(Calendar.MONTH, 2);
+
+        boolean firstLine = true, newMoon = false, firstQuarter = false, fullMoon = false, lastQuarter = false;
+
+        String exactPhase = "";
         while (date.before(until)) {
             // All calculations here
             AstroComputer.calculate(
@@ -60,19 +65,64 @@ public class MoonIllumination {
                     date.get(Calendar.SECOND));
             double moonIllum = AstroComputer.getMoonIllum();
             double moonPhase = AstroComputer.getMoonPhase();
+            if (moonPhase > 270) {
+                if (!lastQuarter) {
+                    exactPhase = firstLine ? "" : "Last Quarter";
+                    newMoon = false;
+                    firstQuarter = false;
+                    fullMoon = false;
+                    lastQuarter = true;
+                } else {
+                    exactPhase = "";
+                }
+            } else if (moonPhase > 180) {
+                if (!fullMoon) {
+                    exactPhase = firstLine ? "" : "Full Moon";
+                    newMoon = false;
+                    firstQuarter = false;
+                    fullMoon = true;
+                    lastQuarter = false;
+                } else {
+                    exactPhase = "";
+                }
+            } else if (moonPhase > 90) {
+                if (!firstQuarter) {
+                    exactPhase = firstLine ? "" : "First Quarter";
+                    newMoon = false;
+                    firstQuarter = true;
+                    fullMoon = false;
+                    lastQuarter = false;
+                } else {
+                    exactPhase = "";
+                }
+            } else if (moonPhase > 0) {
+                if (!newMoon) {
+                    exactPhase = firstLine ? "" : "New Moon";
+                    newMoon = true;
+                    firstQuarter = false;
+                    fullMoon = false;
+                    lastQuarter = false;
+                } else {
+                    exactPhase = "";
+                }
+            }
             if (csv) {
-                System.out.println(String.format("%s\t%05.02f\t%06.02f",
+                System.out.println(String.format("%s\t%07.04f\t%06.02f\t%s",
                         SDF_UTC.format(date.getTime()),
                         moonIllum,
-                        moonPhase));
+                        moonPhase,
+                        exactPhase));
             } else {
-                System.out.println(String.format("%s - Moon Illumination: %05.02f%% - Phase %06.02f\272",
+                System.out.println(String.format("%s - Moon Illumination: %07.04f%% - Phase %06.02f\272 %s",
                         SDF_UTC.format(date.getTime()),
                         moonIllum,
-                        moonPhase));
+                        moonPhase,
+                        exactPhase));
             }
             // Increment by 1 hour
-            date.add(Calendar.HOUR_OF_DAY, 1);
+//            date.add(Calendar.HOUR_OF_DAY, 1);
+            date.add(Calendar.MINUTE, 10);
+            firstLine = false;
         }
     }
 }
