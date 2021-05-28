8d035d77b44ced1956fa27ef26d3a23ca1169cde
olivier.lediouris <olivier.lediouris@gmail.com>
2021-05-28 07:29:51 -0700

    Bug 2038

--- src/main/java/celestial/almanac/Bug2038.java
+++ src/main/java/celestial/almanac/Bug2038.java
@@ -28,8 +28,12 @@ public class Bug2038 {
         Date maxDate = new Date(Long.MAX_VALUE);
         System.out.println(String.format("Max Date (long): %s", SDF_UTC.format(maxDate)));
 
-        maxDate = new Date(Integer.MAX_VALUE);
-        System.out.println(String.format("Max Date (integer): %s", SDF_UTC.format(maxDate)));
+//        maxDate = new Date(Integer.MAX_VALUE);
+//        System.out.println(String.format("Max Date (integer): %s", SDF_UTC.format(maxDate)));
+
+        maxDate = new Date(1_000L * Integer.MAX_VALUE);
+        System.out.printf("%s seconds after 1970-01-01 00:00:00:\n", NumberFormat.getInstance().format(Integer.MAX_VALUE));
+        System.out.println(String.format("Max Date (integer * 1000): %s <- This is where Unix might have problems...", SDF_UTC.format(maxDate)));
 
     }
 }
